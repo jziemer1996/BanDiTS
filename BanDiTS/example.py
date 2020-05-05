d9753f910f8a6d8b3164bb6bc7dc552a8c637238
jziemer1996 <jz199623@gmail.com>
2020-05-05 21:14:37 +0200

    added water_functions.py

--- BanDiTS/example.py
+++ BanDiTS/example.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from BanDiTS import preprocessing, apply_along_axis, export_arr
 from BanDiTS.statistical_functions import *
 from BanDiTS.filter_functions import *
+from BanDiTS.water_functions import *
 from BanDiTS.breakpoint_functions import *
 from pathos import multiprocessing as mp
 
@@ -12,24 +13,24 @@ def main():
     ###################################     INPUT    ########################################
 
     # Input Folder:
-    raster_folder = "D:/HiWi/01_SALDI/Layerstacks/Pilanesberg/13_eskom_powerlines_jonas/"
+    # raster_folder = "D:/HiWi/01_SALDI/Layerstacks/Pilanesberg/13_eskom_powerlines_jonas/"
     # raster_folder = "D:/HiWi/AAA_GEO402_Daten/"
 
     # raster_folder = "D:/HiWi/01_SALDI/Output_Augrabies/"
     # raster_folder = "D:/HiWi/01_SALDI/Output_Mokala/"
-    # raster_folder = "D:/HiWi/01_SALDI/Output_Mpumalanga/"
+    raster_folder = "D:/HiWi/01_SALDI/Output_Mpumalanga/"
     # raster_folder = "D:/HiWi/01_SALDI/Output_Freestate/"
     # raster_folder = "D:/HiWi/01_SALDI/Output_Pilanesberg/"
     # raster_folder = "D:/HiWi/01_SALDI/Output_Agulhas/"
 
 
     # Input File Name
-    raster_filename = "S1_A_VH_stack_powerline_subset"
+    raster_filename = "Driekoppies_VH_median_filter3"
 
     ###################################     OUTPUT    ########################################
 
     # Output Folder:
-    output_folder = "D:/HiWi/01_SALDI/Layerstacks/Pilanesberg/13_eskom_powerlines_jonas/"
+    output_folder = "D:/HiWi/01_SALDI/Output_Mpumalanga/"
 
     ####################### USER-DEPENDENT FILTER-FUNCTIONS TO BE USED #######################
     # Example for mean filter:
@@ -37,27 +38,35 @@ def main():
     # filter_args = [{"kernel": 3}, {"kernel": 9}, {"kernel": 13}]
 
     # Example for median filter:
-    filter_functions = [median_filter]
-    filter_args = [{"kernel": 3}]
+    # filter_functions = [median_filter]
+    # filter_args = [{"kernel": 3}]
 
     # Example for Sobel filter:
     # filter_functions = [sobel_filter, sobel_filter, sobel_filter, sobel_filter, sobel_filter]
     # filter_args = [{"kernel": [-5, 0, 5]}, {"kernel": [-5, -5, 0, 5, 5]}, {"kernel": [-5, -5, -5, 0, 5, 5, 5]}, {"kernel": [-5, -5, -5, -5, 0, 5, 5, 5, 5]}, {"kernel": [-5, -5, -5, -5, -5, 0, 5, 5, 5, 5, 5]}]
 
+    filter_functions = [sobel_filter]
+    filter_args = [{"kernel": [-5, -5, -5, -5, -5, 0, 5, 5, 5, 5, 5]}]
+
     ################### USER-DEPENDENT STATISTICAL FUNCTIONS TO BE USED ######################
     # Example for statistical function:
-    statistical_functions = [otsu]
-    statistical_args = [{}]         # take care of name of statistical arguments in export (line 113)
+    statistical_functions = [count_threshold]
+    statistical_args = [{"lower":-23.45, "upper":-22}]         # take care of name of statistical arguments in export (line 113)
+
+    ################### USER-DEPENDENT WATER FUNCTIONS TO BE USED ######################
+    # Example for water function:
+    water_functions = [count_threshold]
+    water_args = [{"lower":-23.45, "upper":-22}]  # take care of name of statistical arguments in export (line 113)
 
     ###################### USER-DEPENDENT BREAKPOINT FUNCTIONS TO BE USED ####################
     # Example for breakpoint functions (APPLY ONLY AFTER MEDIAN- AND SOBEL-FILTER!!!):
     breakpoint_functions = [count_breakpoint]
-    breakpoint_args = [{"threshold": 125}]
+    breakpoint_args = [{"threshold": 70}]
 
     ######################   NO USER INPUT BEYOND THIS POINT   ###############################
 
     return raster_folder, raster_filename, output_folder, filter_functions, filter_args, statistical_functions, \
-        statistical_args, breakpoint_functions, breakpoint_args
+        statistical_args, water_functions, water_args, breakpoint_functions, breakpoint_args
 
 
 def filter_func(raster_folder, raster_filename, output_folder, filter_functions, filter_args):
@@ -110,7 +119,7 @@ def statistics_func(raster_folder, raster_filename, output_folder, statistical_f
     dates = arr[1]
 
     for i, func in enumerate(statistical_functions):
-        #threshold_size = str(statistical_args[i]['threshold_size'])
+        # threshold_size = str(statistical_args[i]['threshold_size'])
         # creating results with calling wanted algorithm in parallel_apply_along_axis for quick runtime
         result = apply_along_axis.parallel_apply_along_axis(func1d=func, arr=arr[0], axis=0,
                                                             cores=mp.cpu_count(), **statistical_args[i])
@@ -123,7 +132,43 @@ def statistics_func(raster_folder, raster_filename, output_folder, statistical_f
         func_name = str(func)[func_name_start:func_name_end]
 
         # exporting result to new raster
-        export_arr.functions_out_array(outname=outname + "_" + func_name + str(), arr=result, input_file=input_raster,
+        export_arr.functions_out_array(outname=outname + "_" + func_name + str(4), arr=result, input_file=input_raster,
+                                       dtype=dtype)
+
+    # print time to this point
+    statistics_time = datetime.now()
+    print("breakpoint-time = ", statistics_time - start_time, "Hr:min:sec")
+
+
+def water_func(raster_folder, raster_filename, output_folder, water_functions, water_args):
+    start_time = datetime.now()
+    input_raster = os.path.join(raster_folder, raster_filename)
+    hdr_file = ""  # input_raster + ".hdr"        # only used for ENVI stacks
+    outname = os.path.join(output_folder, raster_filename)
+    if outname.find(".tif") != -1:
+        outname = outname[0:len(outname) - 4]
+
+    # arr: full size numpy array 3D XxYxZ 200x300x100
+    arr = preprocessing.rio_array(input_raster, hdr_file=hdr_file)
+
+    # activate to get list of dates from .hdr file (.hdr file needs to be specified above)
+    dates = arr[1]
+
+    for i, func in enumerate(water_functions):
+        # threshold_size = str(statistical_args[i]['threshold_size'])
+        # creating results with calling wanted algorithm in parallel_apply_along_axis for quick runtime
+        result = apply_along_axis.parallel_apply_along_axis(func1d=func, arr=arr[0], axis=0,
+                                                            cores=mp.cpu_count(), **water_args[i])
+
+        # selecting dtype based on result
+        dtype = type(result[0][0])
+
+        func_name_end = str(func).find(" at")
+        func_name_start = 10
+        func_name = str(func)[func_name_start:func_name_end]
+
+        # exporting result to new raster
+        export_arr.functions_out_array(outname=outname + "_" + func_name + str(10), arr=result, input_file=input_raster,
                                        dtype=dtype)
 
     # print time to this point
@@ -174,11 +219,17 @@ if __name__ == '__main__':
     #             filter_args=in_variables[4])
 
     # call this function to execute statistics functions:
-    statistics_func(raster_folder=str(in_variables[0]), raster_filename=str(in_variables[1]),
-                    output_folder=str(in_variables[2]), statistical_functions=in_variables[5],
-                    statistical_args=in_variables[6])
+    # statistics_func(raster_folder=str(in_variables[0]), raster_filename=str(in_variables[1]),
+    #                 output_folder=str(in_variables[2]), statistical_functions=in_variables[5],
+    #                 statistical_args=in_variables[6])
+
+    # call this function to execute water functions:
+    water_func(raster_folder=str(in_variables[0]), raster_filename=str(in_variables[1]),
+                    output_folder=str(in_variables[2]), water_functions=in_variables[7],
+                    water_args=in_variables[8])
+
 
     # call this function to execute breakpoint functions:
     # breakpoint_func(raster_folder=str(in_variables[0]), raster_filename=str(in_variables[1]),
-    #                 output_folder=str(in_variables[2]), breakpoint_functions=in_variables[7],
-    #                 breakpoint_args=in_variables[8])
+    #                 output_folder=str(in_variables[2]), breakpoint_functions=in_variables[9],
+    #                 breakpoint_args=in_variables[10])
